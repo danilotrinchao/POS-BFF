d7cedbb738c9e53d9e775074a644d4d2674d428e
danilo.py <danilo.py@outlook.com>
2024-06-27 21:49:44 -0300

    f

--- startBE.bat
+++ startBE.bat
@@ -1,17 +1,17 @@
 @echo off
 setlocal
 
-rem Iniciar API 1
+rem Iniciar API 1 na porta 5000
 cd /d C:\caminho\para\a\api1\publish
-start dotnet NomeDaSuaAPI1.dll
+start dotnet NomeDaSuaAPI1.dll --urls "https://localhost:44363"
 
-rem Iniciar API 2
+rem Iniciar API 2 na porta 5001
 cd /d C:\caminho\para\a\api2\publish
-start dotnet NomeDaSuaAPI2.dll
+start dotnet NomeDaSuaAPI2.dll --urls "https://localhost:7250"
 
-rem Iniciar API 3
+rem Iniciar API 3 na porta 5002
 cd /d C:\caminho\para\a\api3\publish
-start dotnet NomeDaSuaAPI3.dll
+start dotnet NomeDaSuaAPI3.dll --urls "https://localhost:7209"
 
 endlocal
 pause
