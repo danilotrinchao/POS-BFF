d34951a0c8b9fd772431afbcad1a89b2cdebecb4
danilo.py <danilo.py@outlook.com>
2024-06-30 02:33:19 -0300

    f

--- AuthenticationService.Domain/Gateways/Cashier/ICashierOrderServiceGateway.cs
+++ AuthenticationService.Domain/Gateways/Cashier/ICashierOrderServiceGateway.cs
@@ -9,12 +9,7 @@ namespace AuthenticationService.Core.Domain.Gateways.Cashier
 {
     public interface ICashierOrderServiceGateway
     {
-        Task<OrderDto> GetOrderByIdAsync(Guid id);
-        Task<IEnumerable<OrderDto>> GetAllOrdersAsync();
-        Task<Guid> CreateOrderAsync(SaleDTO saleDto);
-        Task<bool> UpdateOrderAsync(Guid id, SaleDTO saleDto);
-        Task<bool> DeleteOrderAsync(Guid id);
-        Task<bool> OpenCashier(decimal InitialBalance, int EmployeerId);
+        Task<Guid> OpenCashier(decimal InitialBalance, int EmployeerId);
         Task<bool> CloseCashier(Guid CashierId);
     }
 
