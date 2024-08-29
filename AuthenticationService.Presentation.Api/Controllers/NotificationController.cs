7dda64cc06539c39bafed2018bf0335f357960ea
danilo.py <danilo.py@outlook.com>
2024-08-28 23:15:07 -0300

    fix: ajuste notificações

--- AuthenticationService.Presentation.Api/Controllers/NotificationController.cs
+++ AuthenticationService.Presentation.Api/Controllers/NotificationController.cs
@@ -25,37 +25,31 @@ namespace AuthenticationService.Presentation.Api.Controllers
         }
 
         [HttpGet("stream")]
-        public async Task<IActionResult> StreamNotifications()
+        public async Task StreamNotifications()
         {
             Response.ContentType = "text/event-stream";
             Response.Headers.Add("Cache-Control", "no-cache");
             Response.Headers.Add("Connection", "keep-alive");
 
             // Loop contínuo para enviar notificações em tempo real
-            while (true)
+            while (!HttpContext.RequestAborted.IsCancellationRequested)
             {
                 // Notificações de produtos
                 var productNotifications = await _saleProductServiceGateway.GetNotifyStockAsync();
                 var servicesNotifications = await _controlTimeService.GetNotifyServiceAsync();
-                foreach (var notification in productNotifications)
-                {
-                    await _notificationPublisher.PublishAsync(notification);
-                    await Task.Delay(500000); // Exemplo: Atraso de 5 segundos entre as notificações
-                }
-                foreach (var notification in servicesNotifications)
-                {
-                    await _notificationPublisher.PublishAsync(notification);
-                    await Task.Delay(500000); // Exemplo: Atraso de 5 segundos entre as notificações
-                }
-             
 
-                await Task.Delay(100000); // Ajuste o intervalo conforme necessário
+                foreach (var notification in productNotifications.Concat(servicesNotifications))
+                {
+                    var message = $"data: {notification}\n\n"; // Formato para SSE
+                    await Response.WriteAsync(message);
+                    await Response.Body.FlushAsync(); // Assegura que a mensagem é enviada imediatamente
+                    await Task.Delay(5000); // Ajuste o atraso conforme necessário
+                }
+
+                await Task.Delay(10000); // Ajuste o intervalo conforme necessário
             }
-
-            // Este return nunca será alcançado, pois o loop acima é contínuo.
-            // Mas o método precisa ter um retorno válido para compilar.
-            return new EmptyResult();
         }
+
 
     }
 }
